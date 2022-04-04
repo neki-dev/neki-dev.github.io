eb0aa7f8fe1112732e1c39b42ac12561cd9d7840
neki-dev <dev@neki.guru>
2022-04-05 00:40:33 +0300

    Minor fixes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import solidSvg from 'vite-plugin-solid-svg';
+import SVGInjectPlugin from 'vite-plugin-svg-inject';
 import alias from 'alias-reuse';
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
-    solidSvg(),
+    SVGInjectPlugin(),
   ],
   root: './src',
   build: {
