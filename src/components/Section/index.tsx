ceeaca46bb8c6ab2e62b83cfd611cb457bb20d85
neki-dev <dev@neki.guru>
2023-01-16 00:39:51 +0300

    Updated section title styles

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -22,12 +22,14 @@ export const Section: Component<Props> = (props) => {
   return (
     <section ref={props.ref} class={props.class}>
       {props.layers}
+
       <div class="wrapper">
         {props.label && (
           <div class="section-label">
             <h4 ref={refLabel}>{props.label}</h4>
           </div>
         )}
+
         {props.children}
       </div>
     </section>
