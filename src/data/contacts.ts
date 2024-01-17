6b0786e89e78c14bc45fc05d29cb4022fe785571
neki-dev <dev@neki.guru>
2024-01-17 21:41:10 +0100

    Removed StackOverflow link

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -4,5 +4,4 @@ export const SOCIAL_CONTACTS: Contact[] = [
   { name: 'Telegram', link: 'https://telegram.me/neki_me' },
   { name: 'LinkedIn', link: 'https://linkedin.com/in/neki' },
   { name: 'GitHub', link: 'https://github.com/neki-dev' },
-  { name: 'StackOverflow', link: 'https://ru.stackoverflow.com/users/15622/nikita-galadiy' },
 ];
